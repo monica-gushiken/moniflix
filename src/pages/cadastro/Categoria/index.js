0a827a8cdc79a1db77da3f2b8b4ae59b2f0657a2
Monica Mayumi Gushiken <monica.gushiken@gmail.com>
2020-07-30 21:56:21 -0300

    Ajuste url servidor ou local

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -29,7 +29,8 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
+    const URL = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categorias' : 'https://moniflix.herokuapp.com/categorias';
     fetch(URL)
       .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
